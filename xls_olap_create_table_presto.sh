04065be224f21e5ad36beb0a1b8d98fe4a9a65e8
lsh <1922627634@qq.com>
2026-03-18 17:07:01 +0800

    版本2

--- xls_olap_create_table_presto.sh
+++ xls_olap_create_table_presto.sh
@@ -8,21 +8,15 @@ fi
 
 PRESTO_HOME=/export/server/presto/bin/presto
 ${PRESTO_HOME} --catalog mysql \
-               --server 192.168.88.80:3306 \
-			   #--schema yp_rpt
-			   #--execute "select * from yp_dws.dws_sale_daycount limit 10;"
+               --server 192.168.88.80:8090 \
 			   --file /mycode/create_rpt_sale_tables_presto.sql
 			   
 			   
 ${PRESTO_HOME} --catalog hive \
-               --server 192.168.88.80:3306 \
-			   #--schema yp_rpt
-			   #--execute "select * from yp_dws.dws_sale_daycount limit 10;"
-			   --file /mycode/create_rpt_sku_tables-presto.sql
+               --server 192.168.88.80:8090 \
+			   --file /mycode/create_rpt_sku_tables_presto.sql
 			   
 			   
 ${PRESTO_HOME} --catalog hive \
-               --server 192.168.88.80:3306 \
-			   #--schema yp_rpt
-			   #--execute "select * from yp_dws.dws_sale_daycount limit 10;"
+               --server 192.168.88.80:8090 \
 			   --file /mycode/create_rpt_user_tables_presto.sql
--- xls_olap_presto_to_mysql.sh
+++ xls_olap_presto_to_mysql.sh
@@ -8,7 +8,5 @@ fi
 
 PRESTO_HOME=/export/server/presto/bin/presto
 ${PRESTO_HOME} --catalog mysql \
-               --server 192.168.88.80:3306 \
-			   #--schema yp_rpt
-			   #--execute "select * from yp_dws.dws_sale_daycount limit 10;"
-			   --file /mycodeexport_presto2mysql_presto.sql
+               --server 192.168.88.80:8090 \
+			   --file /mycode/export_presto2mysql_presto.sql
